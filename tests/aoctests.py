fd4da477bd8813934fc8d8fda6fbf310add7816f
mckirk <mckirk@posteo.de>
2024-12-14 07:30:32 +0100

    turn tests into actual unit tests

--- tests/aoctests.py
+++ tests/aoctests.py
@@ -1,143 +1,139 @@
-from pprint import pprint
+import unittest
 from aocparser import parse
 
 
-def test1():
-    example = """\
+class TestAocParser(unittest.TestCase):
+    def test_simple(self):
+        example = """\
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"""
 
-    spec = "[Card {key:i}: {p1:il} `| {p2:il}|\n]"
+        spec = "[Card {key:i}: {p1:il} `| {p2:il}]"
 
-    parsed = parse(spec, example)
+        parsed = parse(spec, example)
 
-    pprint(parsed)
+        expected = {
+            1: {
+                "key": 1,
+                "p1": [41, 48, 83, 86, 17],
+                "p2": [83, 86, 6, 31, 17, 9, 48, 53],
+            },
+            2: {
+                "key": 2,
+                "p1": [13, 32, 20, 16, 61],
+                "p2": [61, 30, 68, 82, 17, 32, 24, 19],
+            },
+        }
 
-    assert parsed == {
-        1: {"key": 1, "p1": [41, 48, 83, 86, 17], "p2": [83, 86, 6, 31, 17, 9, 48, 53]},
-        2: {
-            "key": 2,
-            "p1": [13, 32, 20, 16, 61],
-            "p2": [61, 30, 68, 82, 17, 32, 24, 19],
-        },
-    }
+        self.assertEqual(parsed, expected)
 
-
-def test2():
-    example = """\
+    def test_nested(self):
+        example = """\
 seeds: 79 14 55 13
 
 seed-to-soil map:
 50 98 2
 52 50 48"""
 
-    spec = """\
+        spec = """\
 seeds: {seeds:il}
 
 {maps:[{key:w}-to-{to:w} map:
-{vals:[{il}|\n]}|\n\n]}"""
+{vals:[{il}|\n]}]}"""
 
-    parsed = parse(spec, example)
+        parsed = parse(spec, example)
 
-    pprint(parsed)
+        expected = {
+            "maps": {
+                "seed": {
+                    "key": "seed",
+                    "to": "soil",
+                    "vals": [[50, 98, 2], [52, 50, 48]],
+                }
+            },
+            "seeds": [79, 14, 55, 13],
+        }
 
-    assert parsed == {
-        "maps": {
-            "seed": {"key": "seed", "to": "soil", "vals": [[50, 98, 2], [52, 50, 48]]}
-        },
-        "seeds": [79, 14, 55, 13],
-    }
+        self.assertEqual(parsed, expected)
 
-
-def test3():
-    example = """\
+    def test_simple2(self):
+        example = """\
 RL
 
 AAA = (BBB, CCC)
 BBB = (DDD, EEE)"""
 
-    spec = """\
+        spec = """\
 {w}
 
-[{key:w} = ({L:w}, {R:w})|\n]"""
+[{key:w} = ({L:w}, {R:w})]"""
 
-    parsed = parse(spec, example)
+        parsed = parse(spec, example)
 
-    pprint(parsed)
+        expected = [
+            "RL",
+            {
+                "AAA": {"L": "BBB", "R": "CCC", "key": "AAA"},
+                "BBB": {"L": "DDD", "R": "EEE", "key": "BBB"},
+            },
+        ]
 
-    assert parsed == [
-        "RL",
-        {
-            "AAA": {"L": "BBB", "R": "CCC", "key": "AAA"},
-            "BBB": {"L": "DDD", "R": "EEE", "key": "BBB"},
-        },
-    ]
+        self.assertEqual(parsed, expected)
 
+    def test_choice(self):
+        spec = "[<mask:mask = {w}|mem:mem`[{addr:i}`] = {val:i}>]"
 
-def test4():
-    spec = "[<mask:mask = {w}|mem:mem`[{addr:i}`] = {val:i}>|\n]"
-
-    inp = """\
+        inp = """\
 mask = 00
 mem[1] = 1
 mask = 01
 mem[2] = 2"""
 
-    parsed = parse(spec, inp)
+        parsed = parse(spec, inp)
 
-    pprint(parsed)
+        expected = [
+            {"mask": "00", "mem": None},
+            {"mask": None, "mem": {"addr": 1, "val": 1}},
+            {"mask": "01", "mem": None},
+            {"mask": None, "mem": {"addr": 2, "val": 2}},
+        ]
 
-    assert parsed == [
-        {"mask": "00", "mem": None},
-        {"mask": None, "mem": {"addr": 1, "val": 1}},
-        {"mask": "01", "mem": None},
-        {"mask": None, "mem": {"addr": 2, "val": 2}},
-    ]
+        self.assertEqual(parsed, expected)
+        self.assertEqual(parsed[0]["mask"], "00")
+        self.assertEqual(parsed[1]["mem"]["addr"], 1)
 
-    assert parsed[0].mask == "00"
-    assert parsed[1].mem.addr == 1
+    def test_choice_lifting(self):
+        spec = "[<set:{n:w}={v:i}|minus:{n:w}-|plus:{n:w}+>|,]"
 
+        inp = "a=1,b=2,c-,d+"
 
-def test5():
-    spec = "[<set:{n:w}={v:i}|minus:{n:w}-|plus:{n:w}+>|,]"
+        parsed = parse(spec, inp)
 
-    inp = "a=1,b=2,c-,d+"
+        expected = [
+            {"minus": None, "n": "a", "plus": None, "set": {"n": "a", "v": 1}},
+            {"minus": None, "n": "b", "plus": None, "set": {"n": "b", "v": 2}},
+            {"minus": {"n": "c"}, "n": "c", "plus": None, "set": None},
+            {"minus": None, "n": "d", "plus": {"n": "d"}, "set": None},
+        ]
 
-    parsed = parse(spec, inp)
+        self.assertEqual(parsed, expected)
+        self.assertEqual(parsed[0]["n"], "a")
+        self.assertEqual(parsed[2]["n"], "c")
 
-    pprint(parsed)
+    def test_direct_dict(self):
+        spec = "[{key:w}={i}|,]"
+        inp = "a=1,b=2,c=3"
+        parsed = parse(spec, inp)
+        expected = {"a": 1, "b": 2, "c": 3}
+        self.assertEqual(parsed, expected)
 
-    assert parsed == [
-        {"minus": None, "n": "a", "plus": None, "set": {"n": "a", "v": 1}},
-        {"minus": None, "n": "b", "plus": None, "set": {"n": "b", "v": 2}},
-        {"minus": {"n": "c"}, "n": "c", "plus": None, "set": None},
-        {"minus": None, "n": "d", "plus": {"n": "d"}, "set": None},
-    ]
-
-    assert parsed[0].n == "a"
-    assert parsed[2].n == "c"
-
-
-def test6():
-    spec = "[{key:w}={i}|,]"
-
-    inp = "a=1,b=2,c=3"
-
-    parsed = parse(spec, inp)
-
-    pprint(parsed)
-
-    assert parsed == {"a": 1, "b": 2, "c": 3}
-
-
-def main():
-    test1()
-    test2()
-    test3()
-    test4()
-    test5()
-    test6()
+    def test_signed_int(self):
+        spec = "p={si},{si} v={si},{si}"
+        inp = "p=0,4 v=3,-3"
+        parsed = parse(spec, inp)
+        expected = [0, 4, 3, -3]
+        self.assertEqual(parsed, expected)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
